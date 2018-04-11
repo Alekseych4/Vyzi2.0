1d8bb76b469db93e53a7dc977e87c01fca5a79ab
Robert Mulyukov <robert.mulyukov@gmail.com>
2018-04-11 16:36:48 +0300

    Added the info views

--- app/src/main/java/ru/abityrienty/vyzi/MainActivity.java
+++ app/src/main/java/ru/abityrienty/vyzi/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ContentFrameLayout;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         if(!myDBHelper.check_exist_db()){
             myDBHelper.create_db();
         }
+
 
         final Button btn = (Button) findViewById(R.id.btn_introduction);
         btn.setOnClickListener(new View.OnClickListener() {
