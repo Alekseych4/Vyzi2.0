d74400038ce07929ec2d84348cd3e624dca103e7
Alekseych <alekseych@list.ru>
2018-04-12 21:46:18 +0300

    Design Changes

--- app/src/main/java/ru/abityrienty/vyzi/AppSplash.java
+++ app/src/main/java/ru/abityrienty/vyzi/AppSplash.java
@@ -1,6 +1,5 @@
 package ru.abityrienty.vyzi;
 
-
 import android.app.Application;
 import android.os.SystemClock;
 
@@ -11,9 +10,6 @@ public class AppSplash extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-
-
         // Don't do this! This is just so cold launches take some time
         SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));
     }
